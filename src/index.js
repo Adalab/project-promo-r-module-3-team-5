223f0246d1192776a5ce9aaf0bacd3d4fdcf25ea
lauragonzalezcalvo <laura24gc@gmail.com>
2022-12-01 18:51:22 +0100

    Add the rotation of the wand on collapsables

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 
-import { HashRouter } from 'react-router-dom';
+// import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
+  // Lo comento por que sino me da error.
+  //Así es como está ahora
+  /*
+    <HashRouter>
+      <App />
+    </HashRouter>
+  */
+  //Lo he dejado como antes para que no me de error
+  <React.StrictMode>
     <App />
-  </HashRouter>
+  </React.StrictMode>
 );
